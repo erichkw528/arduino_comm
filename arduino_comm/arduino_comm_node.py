90e665867e8ac44e709411bdd6d1fa56e8e11324
Michael Wu <wuxiaohua1011@gmail.com>
2023-07-05 23:56:34 -0700

    lite: communication done

--- arduino_comm/arduino_comm_node.py
+++ arduino_comm/arduino_comm_node.py
@@ -2,79 +2,128 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-from nis import cat
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String, Header
-import serial
-from .arduino_comm import Arduino, VehicleState, Actuation, StatusEnum
 from ackermann_msgs.msg import AckermannDriveStamped
-from roar_msgs.msg import EgoVehicleControl, VehicleStatus
+from roar_msgs.msg import VehicleStatus, EgoVehicleControl
 from typing import Optional
+import socket
+import time
+import struct
+import json
+from pydantic import BaseModel
+from pydantic.types import confloat
+
+
+class ArduinoVehicleStateModel(BaseModel):
+    is_auto: bool = False
+    is_left_limiter_ON: bool = False
+    is_right_limiter_ON: bool = False
+    angle: float = 0.0
+    angular_velocity: float = 0.0
+    speed: float = 0.0
+    steering: float = 0.0
+
+
+class ArduinoCmdActionModel(BaseModel):
+    target_speed: float = 0.0
+    steering_angle: float = 0.0
+    brake: confloat(ge=0.0, le=1.0) = 0.0
+    reverse: bool = False
 
 
 class ArduinoCommNode(Node):
     def __init__(self):
         super().__init__("arduino_comm_node")
-        self.cmd_sub_ = self.create_subscription(
-            EgoVehicleControl, "/arduino/ego_vehicle_control", self.cmd_recvd, 10
-        )
-        self.vehicle_state_publisher_ = self.create_publisher(
-            VehicleStatus, "/arduino/vehicle_status", 10
-        )
-        self.declare_parameter("write_period", 0.1)
-        self.declare_parameter("read_period", 0.1)
-        self.declare_parameter("port", "/dev/ttyACM0")
-        self.declare_parameter("timeout", 0.1)
+        self.declare_parameter("ip_address", "10.42.0.4")
+        self.declare_parameter("port", 1883)
+
+        self.declare_parameter("get_state_period", 0.1)
+        self.declare_parameter("write_action_period", 0.1)
+
         self.declare_parameter("write_timeout", 0.1)
-        self.declare_parameter("baudrate", 115200)
         self.declare_parameter("vehicle_status_header", "base_link")
 
-        write_period: float = (
-            self.get_parameter("write_period").get_parameter_value().double_value
+        # timers
+        get_state_period: float = (
+            self.get_parameter("get_state_period").get_parameter_value().double_value
         )
-        read_period: float = (
-            self.get_parameter("read_period").get_parameter_value().double_value
+        self.get_state_timer = self.create_timer(
+            get_state_period, self.get_state_from_arduino
+        )
+        write_action_period: float = (
+            self.get_parameter("write_action_period").get_parameter_value().double_value
+        )
+        self.write_action_timer = self.create_timer(
+            write_action_period, self.write_action_callback
         )
 
-        self.write_timer = self.create_timer(write_period, self.write_timer_callback)
-        self.read_timer = self.create_timer(read_period, self.read_timer_callback)
+        # pub / sub
+        self.cmd_sub_ = self.create_subscription(
+            EgoVehicleControl, "ego_vehicle_control", self.cmd_recvd, 10
+        )
+        self.vehicle_state_publisher_ = self.create_publisher(
+            VehicleStatus, "vehicle_status", 10
+        )
 
-        self.arduino: Optional[Arduino] = None
-        try:
-            self.arduino = Arduino(
-                port=self.get_parameter("port").get_parameter_value().string_value,
-                timeout=self.get_parameter("timeout")
-                .get_parameter_value()
-                .double_value,
-                write_timeout=self.get_parameter("write_timeout")
-                .get_parameter_value()
-                .double_value,
-                baudrate=self.get_parameter("baudrate")
-                .get_parameter_value()
-                .integer_value,
-            )
-        except Exception as e:
-            self.get_logger().error(f"Unable to initialize arduino: {e}")
-        self.state: Optional[VehicleState] = None
-        self.msg: Optional[EgoVehicleControl] = None
+        self.state: Optional[VehicleStatus] = None
+        self.latest_control_model: ArduinoCmdActionModel = ArduinoCmdActionModel()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def cmd_recvd(self, msg: EgoVehicleControl):
-        self.msg = msg
+        self.latest_control_model = self.p_egoVehicleControlMsgToArduinoCmdActionModel(
+            msg
+        )
+        self.get_logger().debug(f"Recevied: {self.latest_cmd}")
 
-    def write_timer_callback(self):
-        if self.arduino == None:
-            self.get_logger().error("Not writing, Arduino is not initialized")
-            return
-        if self.msg:
-            self._write_msg_to_arduino(self.msg)
+    def get_state_from_arduino(self):
+        message = b"s"
+        self.sock.sendto(message, self.p_getIPAndPort())
 
-    def read_timer_callback(self):
-        curr_state: VehicleState = self._read_state_from_arduino()
-        self._publish_vehicle_state(curr_state=curr_state)
+        data, addr = self.sock.recvfrom(1024)  # Buffer size is 1024 bytes
+        try:
+            string_data = data.decode("utf-8")
+            json_data = json.loads(string_data)
+            latest_model = self.p_dataToVehicleState(json_data)
+            self.p_publish_state(latest_model)
+        except Exception as e:
+            self.get_logger().error(f"Unable to convert [{data}]. Error: {e}")
 
-    def _publish_vehicle_state(self, curr_state: Optional[VehicleState]):
+    def write_action_callback(self):
+        message = json.dumps(self.latest_control_model.json()).encode("utf-8")
+        self.sock.sendto(message, self.p_getIPAndPort())
+        self.get_logger().debug(f"Wrote: {self.latest_control_model}")
+
+    def p_egoVehicleControlMsgToArduinoCmdActionModel(
+        self, egoVehicleControlMsg: EgoVehicleControl
+    ) -> ArduinoCmdActionModel:
+        model = ArduinoCmdActionModel()
+        model.brake = egoVehicleControlMsg.brake
+        model.reverse = egoVehicleControlMsg.reverse
+        model.steering_angle = egoVehicleControlMsg.steering_angle
+        model.target_speed = egoVehicleControlMsg.target_speed
+
+        return model
+
+    def p_getIPAndPort(self):
+        ip_address = self.get_parameter("ip_address").get_parameter_value().string_value
+        port = self.get_parameter("port").get_parameter_value().integer_value
+        return (ip_address, port)
+
+    def p_dataToVehicleState(self, data: dict) -> ArduinoVehicleStateModel:
+        model = ArduinoVehicleStateModel()
+        model.is_auto = data.get("is_auto", False)
+        model.is_left_limiter_ON = data.get("is_left_limiter_ON", False)
+        model.is_right_limiter_ON = data.get("is_right_limiter_ON", False)
+        model.angle = data.get("angle", False)
+        model.angular_velocity = data.get("angular_velocity", False)
+        model.speed = data.get("speed", False)
+        model.steering = data.get("steering", False)
+        return model
+
+    def p_publish_state(self, curr_state: ArduinoVehicleStateModel):
         header = Header()
         header.stamp = self.get_clock().now().to_msg()
         header.frame_id = (
@@ -83,54 +132,16 @@ class ArduinoCommNode(Node):
             .string_value
         )
         msg = VehicleStatus()
-        if curr_state == None:
-            self.get_logger().error(f"Vehicle status is abnormal: [{curr_state}]")
-            msg.header = header
-            self.vehicle_state_publisher_.publish(msg)
-            return
 
-        if curr_state.status != StatusEnum.NORMAL:
-            self.get_logger().error(f"Vehicle status is abnormal: [{curr_state}]")
-            return
+        msg.angle = curr_state.angle
+        msg.is_auto = curr_state.is_auto
+        msg.is_left_limiter_on = curr_state.is_left_limiter_ON
+        msg.is_right_limiter_on = curr_state.is_right_limiter_ON
+        msg.speed = curr_state.speed
 
-        self.state = curr_state
-        msg.angle = self.state.angle
-        msg.is_auto = self.state.is_auto
-        msg.actuation.throttle = self.state.actuation.throttle
-        msg.actuation.steer = self.state.actuation.steering
-        msg.actuation.brake = self.state.actuation.brake
-        msg.actuation.reverse = self.state.actuation.reverse
-        msg.status = 1
         self.vehicle_state_publisher_.publish(msg)
 
-    def _read_state_from_arduino(self) -> Optional[VehicleState]:
-        try:
-            if self.arduino == None:
-                return VehicleState(status=StatusEnum.UNKNOWN_ERROR)
-            state: VehicleState = self.arduino.read_state()
-            return state
-        except Exception as e:
-            self.get_logger().error(f"Error: {e}")
-            return VehicleState(status=StatusEnum.UNKNOWN_ERROR)
-
-    def _write_msg_to_arduino(self, msg: EgoVehicleControl):
-        try:
-            self.arduino.write_actuation(
-                act=Actuation(
-                    throttle=msg.throttle,
-                    steering=msg.steer,
-                    brake=msg.brake,
-                    reverse=msg.reverse,
-                )
-            )
-        except Exception as e:
-            self.get_logger().error(f"Error: {e}")
-
     def destroy_node(self):
-        if self.arduino:
-            self.arduino.write_actuation(
-                actuation=Actuation(speed=0, steering=0, brake=0)
-            )
         super().destroy_node()
 
 
--- setup.py
+++ setup.py
@@ -3,28 +3,31 @@
 # license that can be found in the LICENSE file.
 
 from setuptools import setup
+import os
+from glob import glob
 
-package_name = 'arduino_comm'
+package_name = "arduino_comm"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='michael',
-    maintainer_email='wuxiaohua1011@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="michael",
+    maintainer_email="wuxiaohua1011@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'arduino_comm_node = arduino_comm.arduino_comm_node:main'
-        ],
+        "console_scripts": ["arduino_comm_node = arduino_comm.arduino_comm_node:main"],
     },
 )
